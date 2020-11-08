0adc49c7a2af4e9221995313854c2d804b89e77c
Tanmay <tanmaydeshmukh190@hotmail.com>
2020-11-08 13:44:32 +0530

    Swagger ENabled
    ENabled Swagger

--- src/main/java/com/simplee2ebackendE/Application.java
+++ src/main/java/com/simplee2ebackendE/Application.java
@@ -2,12 +2,30 @@ package com.simplee2ebackendE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.google.common.base.Predicate;
+
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+		.apis((Predicate<RequestHandler>) RequestHandlerSelectors.basePackage("com.simplee2ebackendE"))
+		.paths((Predicate<String>) PathSelectors.any()).build().apiInfo(ApiInfo.DEFAULT);
+	}
 }
